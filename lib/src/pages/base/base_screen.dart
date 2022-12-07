62c488918be03f57b84e8add5f18417fffa3cb75
Isaias Rosa <isaiascarlos99i@gmail.com>
2022-12-07 17:09:47 -0300

    Inserção de animações

--- lib/src/pages/base/base_screen.dart
+++ lib/src/pages/base/base_screen.dart
@@ -13,13 +13,13 @@ class BaseScreen extends StatefulWidget {
 }
 
 class _BaseScreenState extends State<BaseScreen> {
-  int currentIndex =0;
+  int currentIndex = 0;
   final pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:PageView(
+      body: PageView(
         physics: const NeverScrollableScrollPhysics(),
         controller: pageController,
         children: const [
@@ -30,36 +30,31 @@ class _BaseScreenState extends State<BaseScreen> {
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: currentIndex,
-        onTap: (index) {
-          setState(() {
-            currentIndex = index;
-            pageController.jumpToPage(index);
-          });
-        },
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.green,
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.white.withAlpha(100),
-        items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home_outlined),
-          label: 'Home'
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.shopping_cart_outlined),
-          label: 'Carrinho'
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.list),
-          label: 'Pedidos'
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.person_outline),
-          label: 'Perfil'
-        ),
-        
-      ]),
+          currentIndex: currentIndex,
+          onTap: (index) {
+            setState(() {
+              currentIndex = index;
+              //pageController.jumpToPage(index);
+              pageController.animateToPage(
+                index,
+                duration: const Duration(milliseconds: 400),
+                curve: Curves.easeInCirc,
+              );
+            });
+          },
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.green,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.white.withAlpha(100),
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_cart_outlined), label: 'Carrinho'),
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Pedidos'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person_outline), label: 'Perfil'),
+          ]),
     );
   }
 }
